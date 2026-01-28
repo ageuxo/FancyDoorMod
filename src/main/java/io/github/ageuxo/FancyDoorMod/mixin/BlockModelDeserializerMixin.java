3558d193475cf4693646583e0e5a6179fe2e7915
ageuxo <ageuxo@gmail.com>
2026-01-28 17:35:30 +0100

    Fix mixined list being immutable

--- src/main/java/io/github/ageuxo/FancyDoorMod/mixin/BlockModelDeserializerMixin.java
+++ src/main/java/io/github/ageuxo/FancyDoorMod/mixin/BlockModelDeserializerMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -19,7 +20,7 @@ public abstract class BlockModelDeserializerMixin {
     @Inject(method = "getElements", at = @At("HEAD"), cancellable = true)
     protected void fancydoors$getElementsInject(JsonDeserializationContext context, JsonObject json, CallbackInfoReturnable<List<BlockElement>> cir) {
         if (json.has("loader") && Objects.equals(json.get("loader").getAsString(), GroupGeometryLoader.ID.toString())) {
-            cir.setReturnValue(List.of());
+            cir.setReturnValue(new ArrayList<>());
         }
     }
 
